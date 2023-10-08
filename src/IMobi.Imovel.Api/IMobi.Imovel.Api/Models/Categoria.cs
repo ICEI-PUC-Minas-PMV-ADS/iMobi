340f13709d21bea968f40b366c58ec6c5815c582
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-10-07 22:23:23 -0300

    creating models

---
+++ src/IMobi.Imovel.Api/IMobi.Imovel.Api/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace IMobi.Imovel.Api.Models;
+
+public class Categoria
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Icone { get; set; }
+}
---
+++ src/IMobi.Imovel.Api/IMobi.Imovel.Api/Models/Propriedade.cs
@@ -0,0 +1,38 @@
+﻿namespace IMobi.Imovel.Api.Models;
+
+public class Propriedade
+{
+    public int Id { get; set; }
+    public FinalidadeEnum Finalidade { get; set; }
+    public StatusEnum Status { get; set; }
+    public int AreaPrivativa { get; set; }
+    public int AreaTotal { get; set; }
+    public int Quartos { get; set; }
+    public int Suites { get; set; }
+    public GaragemEnum Garagem { get; set; }
+    public string Detalhes { get; set; }
+    public decimal Preco { get; set; }
+    public decimal ValorCondominio { get; set; }
+    public decimal ValorAluguel { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
+
+public enum FinalidadeEnum
+{
+    Venda,
+    Aluguel
+}
+
+public enum StatusEnum
+{
+    Lancamento,
+    Pronto,
+    EmConstrucao
+}
+
+public enum GaragemEnum
+{
+    Sim,
+    Nao
+}
