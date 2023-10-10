bd784a855f69b3cfa79072b569bd2175f536c4f4
Rafael Souza <rafael.souza54@hotmail.com>
2023-10-09 22:23:06 -0300

    criação da model da galeria de imagens

---
+++ src/microservice/ImagesMS/Models/Images.cs
@@ -0,0 +1,12 @@
+namespace ImagesMS.Models;
+public class Images{
+    public int id { get; set;}
+    public string PropriedadeId { get; set;}
+    public string CaminhoImagem { get; set;}
+    public string Detalhes { get; set;}
+    public int Ordem { get; set;}
+    public string CreateAt { get; set;}
+    public string UpdateAt { get; set;}
+    
+
+}
